552e92446527b3ff8ef67e6109b0fba697cba2d3
Hugh McGee <hugh41971@gmail.com>
2020-09-30 16:30:32 -0400

    fixed while loop at line 25

--- Program.cs
+++ Program.cs
@@ -11,47 +11,42 @@ namespace Choose_Your_Class
             Console.WriteLine("You've just had a long,fun day playing with that nifty Virtual Pet you just made!");
             Console.WriteLine("Those little scamps sure are fun. But so messy! Robots especially - oil everywhere!");
             Console.WriteLine("Let's use our handy - dandy Virtual Handwash Station to clean you up");
-            Console.WriteLine("Would you like to :");
-            Console.WriteLine("1.Run the faucets?");
-            Console.WriteLine("2.Get some soap from the dispenser?");
-            Console.WriteLine("3.Use some towels?");
-            Console.WriteLine("4.Use the blow dryer?");
-            Console.WriteLine("5.Refill the soap and towels before you go?");
-            Console.WriteLine("6.Do nothing and live your life your way?");
-
-            int response = Convert.ToInt32(Console.ReadLine());
             bool keepWashing = true;
             while (keepWashing)
             {
+                Console.WriteLine("Would you like to :");
+                Console.WriteLine("1.Run the faucets?");
+                Console.WriteLine("2.Get some soap from the dispenser?");
+                Console.WriteLine("3.Use some towels?");
+                Console.WriteLine("4.Use the blow dryer?");
+                Console.WriteLine("5.Refill the soap and towels before you go?");
+                Console.WriteLine("6.Do nothing and live your life your way?");
+
+                int response = Convert.ToInt32(Console.ReadLine());
                 if (response == 1)
                 {
                     stationOne.RunFaucets();
-                    break;
                 }
                 else if (response == 2)
                 {
                     stationOne.PumpSoap();
-                    break;
                 }
                 else if (response == 3)
                 {
                     stationOne.PullTowels();
-                    break;
-                }
+                 }
                 else if (response == 4)
-                {
-                    stationOne.UseDryer();
-                    break;
+                 {
+                     stationOne.UseDryer();
                 }
                 else if (response == 5)
                 {
                     stationOne.RefillSupplies();
-                    break;
                 }
                 else
                 {
                     Console.WriteLine("You do you, man. Rock on!");
-                    break;
+                    keepWashing = false;
                 }
 
             }
