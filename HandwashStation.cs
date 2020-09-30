847f79edae4c194773e63ac9c5d3599b57676aca
Hugh McGee <hugh41971@gmail.com>
2020-09-30 16:57:36 -0400

    changed usedryer, property water level

--- HandwashStation.cs
+++ HandwashStation.cs
@@ -8,34 +8,44 @@ namespace Choose_Your_Class
     public class HandwashStation
 
     {
-        public string Faucets { get; set; }
-        public int SoapDispenser { get; set; } = 50;
-        public int TowelDispenser { get; set; } = 100;
+        public int WaterLevel { get; set; } = 70;
+        public int SoapLevel { get; set; } = 50;
+        public int TowelQuantity { get; set; } = 100;
         public string BlowDryer { get; set; }
-        
+        public bool HandsWet { get; set; } = false;
         public void RunFaucets()
         {
+            HandsWet = true;
+            WaterLevel -= 10;
             Console.WriteLine("You ran the faucets!");
         }
         public void PumpSoap()
         {
-            SoapDispenser -= 5;
+            SoapLevel -= 5;
             Console.WriteLine("Here's some soap for you! Scrub up!");
             
         }
         public void PullTowels()
         {
-            TowelDispenser -= 10;
+            TowelQuantity -= 10;
             Console.WriteLine(" You grabbed down some towels! Don't go crazy!");
         }
         public void UseDryer()
         {
-            Console.WriteLine("You used the dryer!Man, it blows hot!");
+            if (HandsWet)
+            {
+                Console.WriteLine("You used the dryer!Man, it blows hot!");
+                HandsWet = false;
+            }
+            else
+            {
+                Console.WriteLine("Your hands are dry!");
+            }
         }
         public void RefillSupplies()
         {
-            SoapDispenser = 50;
-            TowelDispenser = 100;
+            SoapLevel = 50;
+            TowelQuantity = 100;
             Console.WriteLine("You refilled everything! You're a peach!");
         }
     }
